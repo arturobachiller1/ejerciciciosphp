4d12de814f84c30d78a17754e60d8b0b023ff83d
your Name <bachiller.arturo.smr@gmail.com>
2020-09-23 17:31:57 +0100

    Intento de ejercicio 1

--- ejercicios/01.php
+++ ejercicios/01.php
@@ -1,11 +1,34 @@
 <!DOCTYPE html>
 <html>
+<head>
+</head>
+<body>
 
-<?php 
+<?php
+
 $nombre = "Arturo";
 $apellidos = "bachiller";
 $edad = "20";
 $telefono = "789789789";
 $email = "bachiller.arturo.smr@gmail.com";
-echo "$nombre , $apellidos, $edad, $telefono, $email";
+
+echo "<table border=1>";
+echo "<tr>";
+echo "<th> nombre </th>";
+echo "<th> apellidos </th>";
+echo "<th> edad </th>";
+echo "<th> telefono </th>";
+echo "<th> email </th>";
+echo "</tr>";
+echo "<tr>";
+echo "<td> " . $nombre . "</td>";
+echo "<td> " . $apellidos . "</td>";
+echo "<td> " . $edad . "</td>";
+echo "<td> " . $telefono . "</td>";
+echo "<td> " . $nombre . "</td>";
+echo "</tr>";
+echo "</table>";
 ?>
+
+</body>
+</html>
