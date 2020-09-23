56b47d79be8b6cb661fe857deb11b057a0b00abe
your Name <bachiller.arturo.smr@gmail.com>
2020-09-23 17:46:24 +0100

    ejercicio 2

---
+++ ejercicios/02.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+
+$numero = 8;
+echo "<table>";
+
+for ($i = 1; $i<=10; $i++){
+
+    echo "<td> " . $numero * $i . "</td>";
+    
+
+}
+echo "</table>";
+?>
+
+</body>
+</html>
