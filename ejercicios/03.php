6c5dc08e61acc4a4b49ac39c27e39ea6c14c81a4
your Name <bachiller.arturo.smr@gmail.com>
2020-09-25 14:46:58 +0100

    ejercicio 2

---
+++ ejercicios/03.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+
+    for ($numero = 1; $numero<=10;$numero++){
+
+        if ($numero % 3 == 0){
+            echo "$numero ";
+        }
+
+    }
+
+?>
+
+</body>
+</html>
