cf7ae4ae2ddc9e87e9686030ca68925778eb7065
your Name <bachiller.arturo.smr@gmail.com>
2020-09-30 15:29:01 +0100

    Ejercicio 7

--- ejercicios/07.php
+++ ejercicios/07.php
@@ -6,17 +6,14 @@
 
 <?php
 
-    $fibo = 0;
-    $fibo2 = 1;
-    $fibo3 = 0;
-    for($i=1;$i<=10;$i++){
+   $array = ["España", "Francia", "Alemania", "Noruega"];
 
-        $fibo3 = $fibo1 + $fibo2;
-        echo "$fibo3, $fibo, $fibo2";
-        $fibo = $fibo2 + $fibo3;
-        $fibo2 = $fibo3 + $fibo;
-       
-    }
+   foreach ($array as $elemento){
+
+    echo "$elemento ";
+
+   }
+
 
 
 ?>
