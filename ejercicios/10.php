f170c9f2386e4d938cdc5cbacf0a386b8af37f8c
your Name <bachiller.arturo.smr@gmail.com>
2020-09-30 15:56:31 +0100

    Ejercicio 10

---
+++ ejercicios/10.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+<?php
+
+$array = array (
+    'base' => "curry",
+    'escolta' => 'CR7',
+    'pivot' => 'anthony',
+    'alero' => "Lebron",
+    "ala-pivot" => "Davies"
+
+);
+    
+    echo "<pre>";
+    print_r($array); 
+    echo "</pre>";
+
+?>
+
+</body>
+</html>
